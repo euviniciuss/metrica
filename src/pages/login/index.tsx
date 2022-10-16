90c2884f552ac9436780801f39a75fd04185c5de
euviniciuss <dev.mvsc@gmail.com>
2022-10-15 21:25:48 -0300

    feat: criando estrutura e estilizando página de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,35 +5,37 @@ import './styles.css'
 export function Login() {
   return(
     <main className="container">
-      <section className="logo-container">
-        <img src={Logo} alt="Logo da metrica possuindo 3 linhas azuis" />
-        <h1 className="title">
-          Faça login na <br />
-          plataforma da <span>métrica</span>
-        </h1>
-      </section>
+      <div className="content">
+        <section className="logo-container">
+          <img src={Logo} alt="Logo da metrica possuindo 3 linhas azuis" />
+          <h1 className="title">
+            Faça login na 
+            plataforma da <span>métrica</span>
+          </h1>
+        </section>
 
-      <section>
-        <form>
-          <fieldset>
-            <label>E-mail</label>
-            <input type="text" placeholder='Digite seu email'/>
-          </fieldset>
-          
-          <fieldset>
-            <label>Senha</label>
-            <input type="password" placeholder='Digite sua senha'/>
-          </fieldset>
+        <section className="form-container">
+          <form className="form-content">
+            <fieldset className="fieldset-content">
+              <label>E-mail</label>
+              <input type="text" placeholder='Digite seu email'/>
+            </fieldset>
+            
+            <fieldset className="fieldset-content">
+              <label>Senha</label>
+              <input type="password" placeholder='Digite sua senha'/>
+            </fieldset>
 
-          <a href="./">Esqueci minha senha</a>
+            <a className="forgot-password" href="./">Esqueci minha senha</a>
 
-          <button>Entrar</button>
+            <button className="login-button">Entrar</button>
 
-          <span>
-            Não tem conta? <a href="./">Cadastre-se</a>
-          </span>
-        </form>
-      </section>
+            <span className="register-link">
+              Não tem conta? <a href="./">Cadastre-se</a>
+            </span>
+          </form>
+        </section>
+      </div>
     </main>
   ) 
 }
